a413912f998ef373f4910beb19513ce5cadb8d08
pigplouf <103496137+pigplouf@users.noreply.github.com>
2022-04-19 11:25:24 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,92 @@
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # . # . #
+    . . # . .
+    . . # . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . # # # .
+    # . # . #
+    . . # . .
+    . . # . .
+    . . . . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    # . # . #
+    . . # . .
+    . . # . .
+    . . . . .
+    . . # . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . . . .
+    . . # . .
+    . # # # .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . # . .
+    . # # # .
+    # . # . #
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    # . # . #
+    . . # . .
+    `)
+basic.pause(200)
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+})
